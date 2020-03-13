6207b8e2d6bf13e007779b99485ef660776b9ba4
Daniel Ortiz Saez <danielortizsaez@gmail.com>
2020-03-13 08:29:27 +0100

    ejercicio practica tema1

--- tema1/practica.py
+++ tema1/practica.py
@@ -3,5 +3,22 @@ print 1_variable;
 
 division_decimal = 5 / 2
     print division_decimal
+el resultado debe ser 2.5
 
-variable
+variable-2 == 8
+# Mostrar varuable
+variable-2;;
+
+print hola
+
+            print "Son las " + 2
+
+a = 5
+b = 6
+a_mayor_b = a > b
+# Deve de devolver True
+
+division = 0/0
+print division
+
+print "Hola " % "Múndo"
